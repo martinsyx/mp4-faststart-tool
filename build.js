1e0dce944125dd6e3c88a46bd8ad1525f7491510
martin.song <martin.song@lianwei.com.cn>
2026-05-23 19:34:58 +0800

    Fix 404: use index.html as entry, remove rewrite

--- build.js
+++ build.js
@@ -3,12 +3,12 @@ const { join } = require("node:path");
 
 const root = __dirname;
 const outDir = join(root, "public");
-const files = ["faststart.html", "faststart.js"];
 
 async function build() {
   await rm(outDir, { recursive: true, force: true });
   await mkdir(outDir, { recursive: true });
-  await Promise.all(files.map((f) => copyFile(join(root, f), join(outDir, f))));
+  await copyFile(join(root, "faststart.html"), join(outDir, "index.html"));
+  await copyFile(join(root, "faststart.js"), join(outDir, "faststart.js"));
 }
 
 build().catch((e) => { console.error(e); process.exitCode = 1; });

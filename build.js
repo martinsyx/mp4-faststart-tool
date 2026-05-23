b94ebe4bf98cf0d188babf5d77beb471a95e9ac4
martin.song <martin.song@lianwei.com.cn>
2026-05-23 18:48:38 +0800

    Fix Vercel output directory

---
+++ build.js
@@ -0,0 +1,19 @@
+const { copyFile, mkdir, rm } = require("node:fs/promises");
+const { join } = require("node:path");
+
+const outputDir = join(__dirname, "public");
+const files = ["faststart.html", "faststart.js"];
+
+async function build() {
+  await rm(outputDir, { recursive: true, force: true });
+  await mkdir(outputDir, { recursive: true });
+
+  await Promise.all(
+    files.map((file) => copyFile(join(__dirname, file), join(outputDir, file))),
+  );
+}
+
+build().catch((error) => {
+  console.error(error);
+  process.exitCode = 1;
+});
